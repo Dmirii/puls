5639df2d3fd1c5dd5a3c4a1f42e37cecf1a37611
Dima A <dimaa@dimaa.ru>
2020-05-23 08:38:23 +0300

    +add promo section

--- gulpfile.js
+++ gulpfile.js
@@ -13,24 +13,21 @@ gulp.task('server', function() {
         }
     });
 
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("*.scss").on('change', browserSync.reload);
-   
-  
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
-//#sourse/scss/
+
 gulp.task('styles', function() {
-    return gulp.src("src/scss/*.+(scss|sass)")
-         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        //.pipe(rename({suffix: '.min', prefix: ''}).on('error', sass.logError) )
-        .pipe(autoprefixer())        
-        .pipe(gulp.dest("css"))
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/scss/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
